310a2459419dfc3d4c37ac6e3e134049e0473f3c
PavWebDeveloper <paveldevjob@gmail.com>
2024-07-18 10:48:00 +0200

    risolto problema con vue per la configurazione del back-office

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,10 +1,19 @@
 <template>
-    <h1>Benvenuto nella pagina Principale</h1>
+    <div>
+        <h1>Benvenuto nella pagina Principale</h1>
+        <h2> grande zio ce sei riuscito</h2>
+    </div>
 </template>
 
 
 <script>
 
+export default {
+    name: "App",
+    components: {
+
+    }
+}
 </script>
 
 <style lang="scss">
